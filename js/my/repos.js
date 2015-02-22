a770eb37dbde76f71781e3640f6fd3d50b4f6502
Nir Boneh <nboneh5921@gmail.com>
2015-02-21 17:26:42 -0700

    done 50/50

--- js/my/repos.js
+++ js/my/repos.js
@@ -1,7 +1,23 @@
 function myRepos(){
 
-    // display my repos in #list
-    // e.g., https://api.github.com/users/doubleshow/repos
+    console.log('listing my repos')    
+    
+    $.get("https://api.github.com/users/nboneh/repos", github, function(data) {
+        var repos = data
+        
+        $.get("/git-jquery/templates/repoList.jade", function(template) {
 
-    alert('todo')
+            // render the template
+            var html = jade.render(template, {items: repos})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+            // load the first repo to view
+            myRepo(repos[0].full_name)
+
+        })
+
+    })
+
 }
